cbcda422de4abe6da0817968c26d9a45f62e6460
sist <sist@DESKTOP-2UN23EM>
2020-04-07 10:04:07 +0900

    innit

---
+++ eHRGit0101/src/eHRGit0101/HelloWorld.java
@@ -0,0 +1,29 @@
+/**
+ *<pre>
+ * eHRGit0101
+ * Class Name : HelloWorld.java
+ * Description : 
+ * Modification Information
+ * 
+ *   수정일      수정자              수정내용
+ *  ---------   ---------   -------------------------------
+ *  2020-04-07           최초생성
+ *
+ * @author 개발프레임웍크 실행환경 개발팀
+ * @since 2020-04-07 
+ * @version 1.0
+ * 
+ *
+ *  Copyright (C) by H.R. KIM All right reserved.
+ * </pre>
+ */
+
+package eHRGit0101;
+
+/**
+ * @author sist
+ *
+ */
+public class HelloWorld {
+
+}
